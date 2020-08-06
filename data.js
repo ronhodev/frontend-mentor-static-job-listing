05dd432c333c589c1238abf77f9eb4e9c261199a
Ronnie Ho <64434055+ronhodev@users.noreply.github.com>
2020-08-05 21:41:39 -0700

    completed challenge

---
+++ data.js
@@ -0,0 +1,164 @@
+export const jobListings = [
+  {
+    logo: 'photosnap',
+    company: 'Photosnap',
+    statuses: ['New!', 'Featured'],
+    title: 'Senior Frontend Developer',
+    postDate: '1d ago',
+    type: ['Full Time','USA only'],
+    role: 'Frontend',
+    level: 'Senior',
+    languages: [
+      'HTML',
+      'CSS',
+      'Javascript'
+    ]
+  },
+  {
+    logo: 'manage',
+    company: 'Manage',
+    statuses: ['New!', 'Featured'],
+    title: 'Fullstack Developer',
+    postDate: '1d ago',
+    type: ['Part Time', 'Remote'],
+    role: 'Fullstack',
+    level: 'Midweight',
+    languages: ['Python', 'React']
+  },
+  {
+    logo: 'account',
+    company: 'Account',
+    statuses: ['New!'],
+    title: 'Junior Frontend Developer',
+    postDate: '2d ago',
+    type: ['Part Time', 'USA only'],
+    role: 'Frontend',
+    level: 'Junior',
+    languages: [
+      'JavaScript',
+      'React',
+      'Sass'
+    ]
+  },
+  {
+    logo: 'myhome',
+    company: 'MyHome',
+    statuses: [],
+    title: 'Junior Frontend Developer',
+    postDate: '5d ago',
+    type: [ 'Contract', 'USA only'],
+    role: 'Frontend',
+    level: 'Junior',
+    languages: [
+      'CSS',
+      'JavaScript'
+    ]
+  },
+  {
+    logo: 'loop-studios',
+    company: 'Loop Studios',
+    statuses: [],
+    title: 'Software Engineer',
+    postDate: '1w ago',
+    type: [
+      'Full Time',
+      'Worldwide'
+    ],
+    role: 'Fullstack',
+    level: 'Midweight',
+    languages: [
+      'JavaScript',
+      'Ruby',
+      'Sass'
+    ]
+  },
+  {
+    logo: 'faceit',
+    company: 'FaceIt',
+    statuses: [],
+    title: 'Junior Backend Developer',
+    postDate: '2w ago',
+    type: [
+      'Full Time',
+      'UK only'
+    ],
+    role: 'Backend',
+    level: 'Junior',
+    languages: [
+      'Ruby',
+      'RoR'
+    ]
+  },
+  {
+    logo: 'shortly',
+    company: 'Shortly',
+    statuses: [],
+    title: 'Junior Developer',
+    postDate: '2w ago',
+    type: [
+      'Full Time',
+      'Worldwide'
+    ],
+    role: 'Frontend',
+    level: 'Junior',
+    languages: [
+      'HTML',
+      'JavaScript',
+      'Sass'
+    ]
+  },
+  {
+    logo: 'insure',
+    company: 'Insure',
+    statuses: [],
+    title: 'Junior Frontend Developer',
+    postDate: '2w ago',
+    type: [
+      'Full Time',
+      'USA only'
+    ],
+    role: 'Frontend',
+    level: 'Junior',
+    languages: [
+      'JavaScript',
+      'Vue',
+      'Sass'
+    ]
+  },
+  {
+    logo: 'eyecam-co',
+    company: 'Eyecam Co.',
+    statuses: [],
+    title: 'Full Stack Engineer',
+    postDate: '3w ago',
+    type: [
+      'Full Time',
+      'Worldwide'
+    ],
+    role: 'Fullstack',
+    level: 'Midweight',
+    languages: [
+      'JavaScript',
+      'Python',
+      'Django'
+    ]
+  },
+  {
+    logo: 'the-air-filter-company',
+    company: 'The Air Filter Company',
+    statuses: [],
+    title: 'Front-end Dev',
+    postDate: '1mo ago',
+    type: [
+      'Part Time',
+      'Worldwide'
+    ],
+    role: 'Frontend',
+    level: 'Junior',
+    languages: [
+      'JavaScript',
+      'React',
+      'Sass'
+    ]
+  }
+];
---
+++ index.js
@@ -0,0 +1,165 @@
+import images from './images/*.svg';
+import { jobListings } from './data';
+
+let filters = [];
+
+function getHeading({ company, statuses }) {
+  return `
+    <div class="job-heading">
+      <h2>${company}</h2>
+      ${statuses.length > 0 ? statuses.map(status => {
+        if (status === 'New!') {
+          return `<span class="status new">${status.toUpperCase()}</span>`;
+        }
+        if (status === 'Featured') {
+          return `<span class="status featured">${status.toUpperCase()}</span>`;
+        }
+      }).join('') : ''}
+    </div>
+  `;
+}
+
+function getPostDetails({ postDate, type }) {
+  const details = [ postDate, ...type ];
+
+  if (!details.length) {
+    return;
+  }
+
+  return `
+    <ul class="job-details">
+      ${details.map(detail => (
+        `<li><span>${detail}</span></li>`
+      )).join('<span>&bull;</span>')}
+    </ul>
+  `;
+}
+
+function getSecondaryDetails({ role, level, languages }) {
+  const details = [role, level, ...languages];
+
+  if (!details.length) {
+    return;
+  }
+
+  return `
+  <ul class="secondary-details">
+    ${details.map(detail => (
+      `<li><button type="button" class="detail-button" name="${detail}">${detail}</button></li>`
+    )).join('')}
+  </ul>
+  `;
+}
+
+function handleFilterSelect(event) {
+  console.log(event.target.name);
+  filters.push(event.target.name);
+  
+  renderElements();
+}
+
+function removeFilter(event) {
+  console.log(event);
+  filters = filters.filter(item => item !== event.target.value);
+
+  renderElements();
+}
+
+function clearAllFilters() {
+  filters.length = 0;
+  renderElements();
+}
+
+function getJobPostings() {
+  const mainDiv = document.querySelector('.main');
+
+  const jobs = jobListings.filter(job => {
+      if (filters.length === 0) {
+        return true;
+      }
+      let show = true;
+      const details = [job.role, job.level, ...job.languages];
+      filters.forEach(filter => {
+        if (!details.includes(filter)) {
+          show = false;
+        }
+      });
+      return show;
+    })
+    .map(job => {
+    return `
+      <article class="job-posting">
+        <img src="./${images[job.logo]}" alt="${job.company} logo" />
+        <section class="main-section">
+          ${getHeading(job)}
+          <a href="#" aria-label=${job.title}><h3>${job.title}</h3></a>
+          ${getPostDetails(job)}
+        </section>
+        <hr />
+        <section class="secondary-section">
+          ${getSecondaryDetails(job)}
+        </section>
+      </article>
+    `;
+  })
+
+  mainDiv.innerHTML = jobs.join('');
+}
+
+function addSelectListeners() {
+  const buttons = document.querySelectorAll('.detail-button');
+  console.log(buttons);
+  buttons.forEach(btn => {
+    btn.onclick = handleFilterSelect;
+  });
+}
+
+function addFiltersBar() {
+  const mainContent = document.querySelector('.main');
+  if (!mainContent) {
+    return;
+  }
+
+  const filterBar = document.createElement('div');
+  filterBar.classList.add('filter-bar');
+
+  const filtersContainer = document.createElement('div');
+  filtersContainer.classList.add('filters-container');
+
+  filters.forEach(item => {
+    const button = document.createElement('button');
+    button.classList.add('filter-button')
+    button.textContent = item;
+
+    const remove = document.createElement('span');
+    remove.classList.add('filter-remove');
+    remove.textContent = 'X';
+    remove.value = item;
+
+    button.onclick = removeFilter;
+
+    button.appendChild(remove);
+
+    filtersContainer.appendChild(button);
+  })
+
+  filterBar.appendChild(filtersContainer);
+
+  const clearButton = document.createElement('button');
+  clearButton.classList.add('clear-button');
+  clearButton.textContent = 'Clear';
+  clearButton.onclick = clearAllFilters;
+  filterBar.appendChild(clearButton);
+
+  mainContent.insertAdjacentElement('afterbegin', filterBar);
+}
+
+function renderElements() {
+  getJobPostings();
+  addSelectListeners()
+  if (filters.length > 0){
+    addFiltersBar();
+  }
+}
+
+renderElements();
